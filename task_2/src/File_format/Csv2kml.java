4683e58d46da5d64a9de0721af3807f7c1b9d739
shiloygilor <shilo.y.gilor@gmail.com>
2018-11-30 14:39:39 +0200

    renamed

--- task_2/src/File_format/Csv2kml.java
+++ task_2/src/File_format/Csv2kml.java
@@ -1,6 +1,6 @@
 package File_format;
 
-import Geom.Point3D;
+import Geom.My_geom_element;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -8,46 +8,37 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
-import java.util.ArrayList;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import java.util.Date;
+
+import GIS.My_GIS_layer;
+import GIS.My_GIS_project;
+import GIS.My_GIS_element;
+import GIS.My_meta_data;
 
 /**
- * 
+ * This class takes a CSV file and converts it to a GIS layer
  * @author Shilo Gilor and Amiel Liberman
  *
  */
-public class Csv2kml {
-
-	public String kmlElement(String MAC, String SSID, String AuthMode, String FirstSeen , String Channel ,String RSSI,String AccuracyMeters, String Type, Point3D point)
+public class Csv2kml 
+{
+/**
+ * 
+ * @param path_of_csv the location that we are taking the csv file
+ * @param path_of_kml_end_point the location that we want the KML file to be saved
+ * @param kml_file_name the name of the name of the KML file requested
+ * @return a fully loaded GIS layer
+ * @throws IOException Exception
+ * @throws NumberFormatException Exception
+ * @throws ParseException Exception
+ */
+	public My_GIS_layer convertCSVToKML(String path_of_csv,String path_of_kml_end_point, String kml_file_name) throws IOException, NumberFormatException, ParseException
 	{
-		return "\t<Placemark>\n" +
-				"\t<name><![CDATA[" + SSID + "]]></name>\n" +
-				"<ExtendedData>\n 	     <Data name=\"MAC\">"
-				+ "  <value>" + MAC + "</value>"
-				+ "    </Data>      <Data name=\"AuthMode\">	  "
-				+ "  <value>" + AuthMode + "</value>"
-				+ "    </Data>      <Data name=\"FirstSeen\">	  "
-				+ "  <value>" + FirstSeen + "</value>"
-				+ "    </Data>      <Data name=\"Channel\">	  "
-				+ "  <value>" + Channel + "</value>"
-				+ "    </Data>      <Data name=\"RSSI\">	  "
-				+ "  <value>" + RSSI + "</value>"
-				+ "    </Data>      <Data name=\"AccuracyMeters\">	  "
-				+ "  <value>" + AccuracyMeters + "</value>"
-				+ "    </Data>      <Data name=\"Type\">	  "
-				+ "  <value>" + Type + "</value>"
-				+ "  </Data>	   "
-				+ " </ExtendedData>"
-				+"<styleUrl>#red</styleUrl>\n"+
-				"\t<Point>\n" +
-				"\t\t<coordinates>"+point.x()+","+point.y()+","+point.z()+ "</coordinates>\n" +
-				"\t</Point>\n" +
-				"\t</Placemark>\n";
-
-	}
-
-	public String createKMLFile(String path) throws IOException
-	{
-		String  kmlmiddle = "";
+		
+		My_GIS_layer gis_layer = new My_GIS_layer(new My_meta_data(new Date().getTime(), null , "green"));	
 		String kmlstart = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
 				"<kml xmlns=\"http://www.opengis.net/kml/2.2\">\n"
 				+ "<Document><Style id=\"red\"><IconStyle><Icon>"
@@ -59,25 +50,25 @@ public class Csv2kml {
 				+ "<Style id=\"green\"><IconStyle><Icon>"
 				+ "<href>http://maps.google.com/mapfiles/ms/icons/green-dot.png</href></Icon>"
 				+ "</IconStyle></Style><Folder><name>Wifi Networks</name>";
-		BufferedReader br = new BufferedReader(new FileReader(path));
+		BufferedReader br = new BufferedReader(new FileReader(path_of_csv));
 		br.readLine();
 		br.readLine();
 		String line = br.readLine();
+		String  kmlmiddle = "";
 		while (line != null && !line.isEmpty()) {
 			if((line.replaceAll(",","")).replaceAll(" ","").isEmpty())
 				break;
 			String[] row = line.split(",");
-			kmlmiddle = kmlmiddle + kmlElement(row[0], row[1], row[2], row[3] , row [4] , row[5], row[9] , row[10], new Point3D(Double.parseDouble(row[7]),Double.parseDouble(row[6]),Double.parseDouble(row[8])));
+			My_GIS_element gis_element = new My_GIS_element(new My_geom_element(Double.parseDouble(row[7]),Double.parseDouble(row[6]),Double.parseDouble(row[8]))
+					, new My_meta_data((new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(row[3])).getTime(), null, "red"),
+					row[0], row[1], row[2], row[4], row[5], row[9] , row[10]);
+			gis_layer.add(gis_element);
+			kmlmiddle = kmlmiddle + gis_element.toStringOfGISElements("50147814");
 			line = br.readLine();
 		}
 		String kmlend = "    </Folder>\n" + 
 				"  </Document> \n </kml>";
-
-		return kmlstart + kmlmiddle + kmlend;
-	}
-	public void convertCSVToKML(String path_of_csv,String path_of_kml_end_point, String kml_file_name) throws IOException
-	{
-		String kmldata = createKMLFile(path_of_csv);
+		String kmldata = kmlstart + kmlmiddle + kmlend;
 		Writer fwriter;
 		try {
 			fwriter = new FileWriter(path_of_kml_end_point+kml_file_name+".kml");
@@ -87,23 +78,7 @@ public class Csv2kml {
 		}catch (IOException e1) {
 			e1.printStackTrace();
 		}   
+		return gis_layer;
 	}
-	public ArrayList<String> multiCSV(String dir) throws IOException
-	{
-		int i=0;
-		ArrayList<String> all_layers = new ArrayList<String>();
-		File[] filesInDirectory = new File(dir).listFiles();
-		for(File f : filesInDirectory)
-		{
-			String filePath = f.getAbsolutePath();
-			String fileExtenstion = filePath.substring(filePath.lastIndexOf(".") + 1,filePath.length());
-			if("csv".equals(fileExtenstion))
-			{
-				convertCSVToKML(filePath, "/Users/shilo/Desktop/", "kmltest_"+i);
-				i++;
-				all_layers.add(createKMLFile(filePath));
-			}
-		}
-		return all_layers;
-	}
+	
 }
--- task_2/src/algorithmnew/MultiCSV.java
+++ task_2/src/algorithmnew/MultiCSV.java
@@ -6,18 +6,34 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+
 import GIS.My_GIS_element;
 import GIS.My_GIS_layer;
 import GIS.My_GIS_project;
 import GIS.My_meta_data;
 import Geom.My_geom_element;
 
+/**
+ * This class takes a path and converts all CSV files in the  to 1 kml file 
+ * @author Shilo Gilor and Amiel Liberman
+ *
+ */
 public class MultiCSV {
-	public My_GIS_layer convertCSVToKML(String path_of_csv) throws IOException
+	/**
+	 * 
+	 * @param path_of_csv the path of the CSV where needed to convert to a GIS layer
+	 * @return a GIS layer full with the data in the CSV file
+	 * @throws IOException Exception
+	 * @throws NumberFormatException Exception
+	 * @throws ParseException Exception
+	 */
+	public My_GIS_layer convertCSVToKML(String path_of_csv) throws IOException, NumberFormatException, ParseException
 	{
 		
-		My_GIS_layer gis_layer = new My_GIS_layer(new My_meta_data(new Date().getTime(), null));	
+		My_GIS_layer gis_layer = new My_GIS_layer(new My_meta_data(new Date().getTime(), null , "7f00ffff"));	
 		BufferedReader br = new BufferedReader(new FileReader(path_of_csv));
 		br.readLine();
 		br.readLine();
@@ -28,17 +44,27 @@ public class MultiCSV {
 				break;
 			String[] row = line.split(",");
 			My_GIS_element gis_element = new My_GIS_element(new My_geom_element(Double.parseDouble(row[7]),Double.parseDouble(row[6]),Double.parseDouble(row[8]))
-					, null, row[0], row[1], row[2], row[3], row[4], row[5], row[9] , row[10]);
+					, new My_meta_data((new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(row[3])).getTime(), null, "red"),
+					row[0], row[1], row[2], row[4], row[5], row[9] , row[10]);
 			gis_layer.add(gis_element);
-			kmlmiddle = kmlmiddle + gis_element.toStringOfGISElements();
+			kmlmiddle = kmlmiddle + gis_element.toStringOfGISElements("50147814");
 			line = br.readLine();
 		}  
 		return gis_layer;
 	}
-	public My_GIS_project multiCSV(String dir,String out_dir,String out_name) throws IOException
+	/**
+	 * 
+	 * @param dir  the path of the files where needed to convert the csv's
+	 * @param out_dir the path of the location to save the KML file
+	 * @param out_name the name to give the KML file
+	 * @return a GIS project full with the data in the file path
+	 * @throws IOException Exception
+	 * @throws NumberFormatException Exception
+	 * @throws ParseException Exception
+	 */
+	public My_GIS_project multiCSV(String dir,String out_dir,String out_name) throws IOException, NumberFormatException, ParseException
 	{
-		My_GIS_project gis_project = new My_GIS_project(null);
-		int i=0;
+		My_GIS_project gis_project = new My_GIS_project(new My_meta_data(new Date().getTime(), null , "7f00ffff"));
 		File[] filesInDirectory = new File(dir).listFiles();
 		for(File f : filesInDirectory)
 		{
@@ -46,8 +72,8 @@ public class MultiCSV {
 			String fileExtenstion = filePath.substring(filePath.lastIndexOf(".") + 1,filePath.length());
 			if("csv".equals(fileExtenstion))
 			{
-				i++;
-				gis_project.add(convertCSVToKML(filePath));
+				My_GIS_layer gis_layer =convertCSVToKML(filePath);
+				gis_project.add(gis_layer);
 			}
 		}
 		Writer fwriter;
